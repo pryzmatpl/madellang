3c4cf1d5f33826885b0ca8f4de59d7802147fb88
Piotr Słupski <piotroxp@gmail.com>
2025-03-19 21:08:33 +0100

    snap

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,10 +8,7 @@ module.exports = {
   },
   style: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+      mode: 'file',
     },
   },
 };
