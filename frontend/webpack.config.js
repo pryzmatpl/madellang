0c66a0a19db20886f5519e0f089bc0875556c3db
Piotr Słupski <piotroxp@gmail.com>
2025-03-19 20:21:19 +0100

    Clean organ donors

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-
 module.exports = {
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-}; 
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+};
